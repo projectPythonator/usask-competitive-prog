fa59bda7abd75167ad723b943d2e2ad44fae71d5
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2023-10-08 12:41:49 -0600

    Update booklet.py
    changed  names to snake case 
    added in some optional variables in __init will list later 
    mutated find_set to be a iterative version. this should help avoid stack overflows and potentially memory overflows from the stack

--- cheatsheetstuff/booklet/booklet.py
+++ cheatsheetstuff/booklet/booklet.py
@@ -1,19 +1,26 @@
 #data structurea
 #union find 
 class UnionFind:
-    def __init__(self, N):
-        self.rank = [0]*N
-        self.p =[i for i in range(N)]
-    
-    def findSet(self, i):
-        if self.p[i]==i:
-            return i
-        else:
-            self.p[i] = self.findSet(self.p[i])
-            return self.p[i]
-    def isSameSet(self, i, j):
-        return (self.findSet(i)==self.findSet(j))
-    def unionSet(self, i, j):
+    def __init__(self, n):
+        self.parents = list(range(n))
+        self.rank  = [0]*n
+        self.sizes = [0]*n
+        self.num_sets = n
+        
+    def find_set(self, u):
+        u_parent = u
+        u_children = []
+        while u_parent != self.parents[u_parent]:
+            u_children.append(u_parent)
+            u_parent = self.parents[u_parent]
+        for child in u_children:
+            self.parents[c] = u_parent
+        return u_parent
+        
+    def is_same_set(self, u, v):
+        return (self.findSet(u)==self.findSet(v))
+
+    def union_set(self, i, j):
         if not self.isSameSet(i, j):
             x = self.findSet(i)
             y = self.findSet(j)
