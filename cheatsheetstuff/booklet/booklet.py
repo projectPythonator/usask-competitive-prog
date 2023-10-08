bcc296462370c1d9f5aeb5c03e8a89bb6b226001
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2023-10-08 13:00:49 -0600

    Update booklet.py
    fixed small bugs and tested it with kruskals on a problem

--- cheatsheetstuff/booklet/booklet.py
+++ cheatsheetstuff/booklet/booklet.py
@@ -3,7 +3,7 @@
 class UnionFind:
     def __init__(self, n):
         self.parents = list(range(n))
-        self.rank  = [0]*n
+        self.ranks  = [0]*n
         self.sizes = [0]*n
         self.num_sets = n
         
@@ -14,19 +14,29 @@ class UnionFind:
             u_children.append(u_parent)
             u_parent = self.parents[u_parent]
         for child in u_children:
-            self.parents[c] = u_parent
+            self.parents[child] = u_parent
         return u_parent
         
     def is_same_set(self, u, v):
-        return (self.findSet(u)==self.findSet(v))
+        return self.find_set(u)==self.find_set(v)
 
-    def union_set(self, i, j):
-        if not self.isSameSet(i, j):
-            x = self.findSet(i)
-            y = self.findSet(j)
-            if self.rank[x]>self.rank[y]:
-                self.p[y]=x
-            else:
-                self.p[x]=y
-                if self.rank[x]==self.rank[y]:
-                    self.rank[y]+=1
+    def union_set(self, u, v):
+        up = self.find_set(u)
+        vp = self.find_set(v)
+        if up==vp:
+            return
+
+        if self.ranks[up] < self.ranks[vp]:
+            self.parents[up] = vp
+            self.sizes[vp] = self.sizes[up]
+        elif self.ranks[vp] < self.ranks[up]:
+            self.parents[vp] = up
+            self.sizes[up] = self.sizes[vp]
+        else:
+            self.parents[vp] = up
+            self.ranks[up] += 1
+            self.sizes[up] += self.sizes[vp]
+        self.num_sets -= 1
+
+    def size_of_u(self, u):
+        return self.sizes[self.find_set(u)]
