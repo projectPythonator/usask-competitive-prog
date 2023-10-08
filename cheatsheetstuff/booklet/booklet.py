8140a325bd80d4c383d279aa34ecd408a727bad5
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2023-10-08 12:23:18 -0600

    Update booklet.py
    added in union find data structure code was translated from c++ implementation 
    note we might find more clean and versatile implementation

--- cheatsheetstuff/booklet/booklet.py
+++ cheatsheetstuff/booklet/booklet.py
@@ -1,1 +1,25 @@
 #data structurea
+#union find 
+class UnionFind:
+    def __init__(self, N):
+        self.rank = [0]*N
+        self.p =[i for i in range(N)]
+    
+    def findSet(self, i):
+        if self.p[i]==i:
+            return i
+        else:
+            self.p[i] = self.findSet(self.p[i])
+            return self.p[i]
+    def isSameSet(self, i, j):
+        return (self.findSet(i)==self.findSet(j))
+    def unionSet(self, i, j):
+        if not self.isSameSet(i, j):
+            x = self.findSet(i)
+            y = self.findSet(j)
+            if self.rank[x]>self.rank[y]:
+                self.p[y]=x
+            else:
+                self.p[x]=y
+                if self.rank[x]==self.rank[y]:
+                    self.rank[y]+=1
