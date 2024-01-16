f6d82b5a00223d9b81dfa37284b0d09eeb1051b2
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-01-16 17:42:11 -0600

    implemented stevens and stanfords chinese remainders theorems

--- cheatsheetstuff/booklet/booklet.py
+++ cheatsheetstuff/booklet/booklet.py
@@ -628,7 +628,7 @@ class GraphAlgorithms:
     def dfs_bipartite_checker(self):
         pass # find code for this later
 
-from math import isqrt, log, gcd
+from math import isqrt, log, gcd, prod
 from itertools import takewhile
 
 class MathAlgorithms:
@@ -839,16 +839,17 @@ class MathAlgorithms:
     def extended_euclid_iterative(self, a, b):
         """Solves coefficients of Bezout identity: ax + by = gcd(a, b), iteratively.
 
-        Complexity per call: Time: O(log n) about twice as fast in python vs above, Space O(1)
+        Complexity per call: Time: O(log n) about twice as fast in python vs above, Space: O(1)
+        Optimizations and notes:
+            divmod and abs are used to help deal with big numbers, remove if < 2**64 for speedup.
         """
-        xx, yy = 0, 1
-        x, y = 1, 0
-        while b != 0:
-            q = a//b
-            a, b = b, a%b
-            x, xx = xx, x-q*xx
-            y, yy = yy, y-q*yy
-        return x, y, a
+        last_remainder, remainder = abs(a), abs(b)
+        x, y, last_x, last_y = 0, 1, 1, 0
+        while remainder:
+            last_remainder, (quotient, remainder) = remainder, divmod(last_remainder, remainder)
+            x, last_x = last_x - quotient * x, x
+            y, last_y = last_y - quotient * y, y
+        return -last_x if a < 0 else last_x, -last_y if b < 0 else last_y, last_remainder
 
     # use in c++ and java
     # use ((a % n) + n) % n for getting proper mod of negative value 
@@ -860,7 +861,7 @@ class MathAlgorithms:
     def modular_linear_equation_solver(self, a, b, n): #needs test
         """Solves gives the solution x in ax = b(mod n).
 
-        Complexity per call: Time: O(log n), Space O(d)
+        Complexity per call: Time: O(log n), Space: O(d)
         """
         x, y, d = self.extended_euclid_recursive(a, n)
         if 0 == b % d:
@@ -869,9 +870,9 @@ class MathAlgorithms:
         return []
 
     def linear_diophantine_1(self, a, b, c):
-        """Solves for x, y in ax + by = c.
+        """Solves for x, y in ax + by = c. From stanford icpc 2013-14
 
-        Complexity per call: Time: O(log n), Space O(1).
+        Complexity per call: Time: O(log n), Space: O(1).
         Notes: order matters? 25x + 18y = 839 != 18x + 25y = 839
         """
         d = gcd(a, b)
@@ -885,47 +886,67 @@ class MathAlgorithms:
         derived from ax + by = c, d = gcd(a, b), and d|c.
         Can further derive into: n = x0 (d/b), and n = y0 (d/a).
 
-        Complexity per call: Time: O(log n), Space O(1).
-        Notes: if a != b order doesn't matter if a == b sometimes they gave the same x, y rather
-        than it being swapped when a, b is swapped.
+        Complexity per call: Time: O(log n), Space: O(1).
+        Optimizations and notes:
+            unlike above this function order doesn't matter if a != b
+            for a speedup call math.gcd(a, b) at start and return accordingly on two lines
         """
-        d = gcd(a, b)
-        if c % d == 0:
-            x, y, d = self.extended_euclid_recursive(a, b)
-            return (x * (c // d)), (y * (c // d))
-        return -1, -1
+        x, y, d = self.extended_euclid_iterative(a, b)
+        return (-1, -1) if c % d != 0 else (x * (c // d), y * (c // d))
 
 
     def mod_inverse(self, b, m): #needs test
         """Solves b^(-1) (mod m).
 
-        Complexity per call: Time: O(log n), Space O(1)
+        Complexity per call: Time: O(log n), Space: O(1)
         """
-        x, y, d = self.extended_euclid_recursive(b, m)
-        return -1 if d != 1 else (x + m) % m
+        x, y, d = self.extended_euclid_iterative(b, m)
+        return None if d != 1 else x % m  # -1 instead of None if we intend to go on with the prog
+
+    def chinese_remainder_theorem_1(self, remainders, modulos):
+        """Steven's CRT version to solve x in x = r[0] (mod m[0]) ... x = r[n-1] (mod m[n-1]).
+
+        Complexity per call: Time O(n log n), Space: O(1)? O(mt) bit size:
+        Optimizations:
+            prod is used from math since 3.8,
+            we use mod mt in the forloop since x might get pretty big.
+        """
+        mt, x = prod(modulos), 0
+        for i, modulo in enumerate(modulos):
+            p = mt // modulo
+            x = (x + (remainders[i] * self.mod_inverse(p, modulo) * p)) % mt
+        return x
+
 
     # stanford icpc 2013-14
-    def crt_helper(self, x, a, y, b): #needs test
-        s, t, d = self.extended_euclid_recursive(x, y)
-        return (0, -1) if a%d != b%d else (self.mod(s*b*x + t*a*y, x*y)//d, x*y//d)
+    def chinese_remainder_theorem_helper(self, mod1, rem1, mod2, rem2): #needs test
+        """Chinese remainder theorem (special case): find z such that z % m1 = r1, z % m2 = r2.
+        Here, z is unique modulo M = lcm(m1, m2). Return (z, M).  On failure, M = -1.
+        from: stanford icpc 2016
+
+        Complexity per call: Time O(log n), Space: O(1)
+        """
+        s, t, d = self.extended_euclid_iterative(mod1, mod2)
+        if rem1 % d != rem2 % d:
+            mod3, sremmod, tremmod = mod1*mod2, s*rem2*mod1, t*rem1*mod2
+            return ((sremmod + tremmod) % mod3) // d, mod3 // d
+        return 0, -1
     
     # from stanford icpc 2013-14
-    def chinese_remainder_theorem(self, x, a):
-        ans = (a[0], x[0])
-        for i in range(1, len(x)):
-            ans = self.crt_helper(ans[1], ans[0], x[i], a[i])
-            if -1 == ans[1]:
+    def chinese_remainder_theorem_2(self, remainders, modulos):
+        """Chinese remainder theorem: find z such that z % m[i] = r[i] for all i.  Note that the
+        solution is unique modulo M = lcm_i (m[i]).  Return (z, M). On failure, M = -1. Note that
+        we do not require the r[i]'s to be relatively prime.
+        from: stanford icpc 2016
+
+        Complexity per call: Time O(n log n), Space: O(1)? O(mt) bit size
+        """
+        z_m = remainders[0], modulos[0]
+        for i, modulo in enumerate(modulos[1:], 1):
+            z_m = self.chinese_remainder_theorem_helper(z_m[1], z_m[0], modulo, remainders[i])
+            if -1 == z_m[1]:
                 break
-        return ans
-    
-    #test this its from stanford icpc 2013-14
-    #computes x and y in ax+by=c failure x=y=-1
-    def linear_diophantine(self, a, b, c):
-        d = gcd(a,b)
-        if c%d == 0:
-            x = c//d * self.mod_inverse(a//d, b//d)
-            return (x, (c - a*x)//b)
-        return (-1, -1)
+        return z_m
 
     def fibonacci_n_iter(self, n):
         self.fibonacci_list = [0] * (n+1)
