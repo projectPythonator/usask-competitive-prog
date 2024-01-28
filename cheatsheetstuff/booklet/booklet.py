9cba24c63ee3d4912d209682835e1846ea34a9f0
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-01-28 10:32:10 -0600

    changed range_sum_query_i_j
    changed to range_sum_from_i_to_j to match previously defined name
    reason
    since it is supppose to functionally do the same as in fenwicktree it whould share the same name might change a bit later but it suppose to functionally do that same thing as in  fewnwicktree

--- cheatsheetstuff/booklet/booklet.py
+++ cheatsheetstuff/booklet/booklet.py
@@ -150,10 +150,10 @@ class RangeUpdateRangeQuery:
         self.point_update_range_query.update_index_by_delta(left, delta * (left - 1))
         self.point_update_range_query.update_index_by_delta(right + 1, -delta * right)
 
-    def range_sum_query_i_j(self, point_i, point_j):  # TODO semi tested
+    def range_sum_from_i_to_j(self, point_i, point_j):  # TODO semi tested
         if point_i > 1:
-            return (self.range_sum_query_i_j(1, point_j)
-                    - self.range_sum_query_i_j(1, point_i - 1))
+            return (self.range_sum_from_i_to_j(1, point_j)
+                    - self.range_sum_from_i_to_j(1, point_i - 1))
         return (self.range_update_point_query.point_sum_query_of_index(point_j) * point_j
                 - self.point_update_range_query.range_sum_from_i_to_j(1, point_j))
 
