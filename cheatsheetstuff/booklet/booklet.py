1a54621df336d3dd7c469ea48b4f240aa31aab30
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-02-05 11:25:39 -0600

    type annotations and naming added
    changed
    is_point_in_radius_of_circle
    rename
    is_point_in_radius_of_circle

--- cheatsheetstuff/booklet/booklet.py
+++ cheatsheetstuff/booklet/booklet.py
@@ -2078,9 +2078,10 @@ class GeometryAlgorithms:  # TODO RETEST
         v = abs(y * b.x + x * b.y + cross_prod)
         return Pt2d((a.x * v + b.x * u) / (v + u), (a.y * v + b.y * u) / (v + u))
 
-    def is_point_p_in_circle_c_radius_r(self, p, c, r):  # TODO RETEST
-        """Computes True if point p in circle False otherwise. Use <= for circumference inclusion"""
-        return self.compare_ab(self.distance_normalized(p, c), r) < 0
+    def is_point_in_radius_of_circle(self, point: Pt2d, center_point: Pt2d, radius: float) -> bool:
+        """True if point p in circle False otherwise. Use <= for circumference inclusion."""
+        # TODO RETEST
+        return self.compare_ab(self.distance_normalized(point, center_point), radius) < 0
 
     def pt_circle_center_given_pt_abc(self, a, b, c):  # TODO RETEST
         """Find the center of a circle based of 3 distinct points
