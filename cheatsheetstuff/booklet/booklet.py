442b67ed6ae7fc552d7f52b834286302ad3eeb19
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-02-10 13:01:50 -0600

    changed ordering to match the src

--- cheatsheetstuff/booklet/booklet.py
+++ cheatsheetstuff/booklet/booklet.py
@@ -1318,16 +1318,27 @@ class MathAlgorithms:
         """Seven variants of prime sieve listed above.
 
         Complexity:
-            function 1: Time: O(n lnln(n)), Space: O(n)
-            function 3: Time: O(n log(n)), Space: O(n)
-            function 2: Time: O(n lnln(n) log(n)), Space: O(n)
+            function 1: Time: O(n log(n)), Space: O(n)
+            function 2: Time: O(n lnln(n)), Space: O(n)
+            function 3: Time: O(n lnln(n) log(n)), Space: O(n)
+            function 4: Time: O(n lnln(n) log(n)), Space: O(n)
         """
         self.euler_phi_plus_sum_and_number_of_diff_prime_factors(n_inclusive)
         self.num_and_sum_of_divisors(n_inclusive)
         self.num_and_sum_of_prime_factors(n_inclusive)
+    def num_and_sum_of_divisors(self, limit: int) -> None:
+        """Does a basic sieve. Complexity function 1."""
+        num_div = [1] * (limit + 1)
+        sum_div = [1] * (limit + 1)
+        for i in range(2, limit + 1):
+            for j in range(i, limit + 1, i):
+                num_div[j] += 1
+                sum_div[j] += i
+        self.num_divisors = num_div
+        self.sum_divisors = sum_div
 
     def euler_phi_plus_sum_and_number_of_diff_prime_factors(self, limit: int) -> None:
-        """This is basically same as sieve just using different ops. Complexity function 1."""
+        """This is basically same as sieve just using different ops. Complexity function 2."""
         num_diff_pf = [0] * (limit + 1)
         sum_diff_pf = [0] * (limit + 1)
         phi = [i for i in range(limit + 1)]
@@ -1341,31 +1352,41 @@ class MathAlgorithms:
         self.sum_diff_prime_factors = sum_diff_pf
         self.euler_phi = phi
 
-    def num_and_sum_of_divisors(self, limit: int) -> None:
-        """Does a basic sieve. Complexity function 2."""
-        num_div = [1] * (limit + 1)
-        sum_div = [1] * (limit + 1)
-        for i in range(2, limit):
-            for j in range(i, limit, i):
-                num_div[j] += 1
-                sum_div[j] += i
-        self.num_divisors = num_div
-        self.sum_divisors = sum_div
-
     def num_and_sum_of_prime_factors(self, limit: int) -> None:
         """This uses similar idea to sieve but avoids divisions. Complexity function 3."""
         num_pf = [0] * (limit + 1)
         sum_pf = [0] * (limit + 1)
-        self.sieve_of_eratosthenes_optimized(limit)
-        for prime in self.primes_list:
-            exponent_limit = int(log(limit, prime)) + 1
-            for exponent in range(1, exponent_limit):
-                prime_to_exponent = prime**exponent
-                for i in range(prime_to_exponent, limit + 1, prime_to_exponent):
-                    sum_pf[i] += prime
-                    num_pf[i] += 1
+        for prime in range(2, limit + 1):
+            if num_pf[prime] == 0:  # or sum_pf if using that one
+                exponent_limit = int(log(limit, prime)) + 1
+                for exponent in range(1, exponent_limit):
+                    prime_to_exponent = prime**exponent
+                    for i in range(prime_to_exponent, limit + 1, prime_to_exponent):
+                        sum_pf[i] += prime
+                        num_pf[i] += 1
         self.num_prime_factors = num_pf
         self.sum_prime_factors = sum_pf
+
+    def num_and_sum_of_divisors_faster(self, limit: int) -> None:
+        """runs in around x0.8-x0.5 the runtime of the slower one. Complexity function 4."""
+        num_divs = [1] * (limit + 1)
+        sum_divs = [1] * (limit + 1)
+        cur_pows = [1] * (limit + 1)
+        for prime in range(2, limit + 1):
+            if num_divs[prime] == 1:
+                exponent_limit = int(log(limit, prime)) + 1
+                for exponent in range(1, exponent_limit):
+                    prime_to_exponent = prime ** exponent
+                    for i in range(prime_to_exponent, limit + 1, prime_to_exponent):
+                        cur_pows[i] += 1
+                tmp = prime - 1  # this line and the line below used for sum_divs
+                prime_powers = [prime ** exponent for exponent in range(0, exponent_limit+1)]
+                for i in range(prime, limit + 1, prime):
+                    num_divs[i] *= cur_pows[i]
+                    sum_divs[i] *= ((prime_powers[cur_pows[i]] - 1) // tmp)
+                    cur_pows[i] = 1
+        self.num_divisors = num_divs
+        self.sum_divisors = sum_divs
 
     def prime_factorize_n(self, n: int) -> List[int]:
         """A basic prime factorization of n function. without primes its just O(sqrt(n))
