f8c5f3e3e4d1e0877e8ee5a843b39cd608177e6a
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-02-12 10:12:56 -0600

    added test_fft_prepare_lengths_list_32

--- cheatsheetstuff/src/MathAlgos/math_algorithms_tests.py
+++ cheatsheetstuff/src/MathAlgos/math_algorithms_tests.py
@@ -402,3 +402,14 @@ class TestMathMethods(unittest.TestCase):
             obj = fast_fourier_transform.MathAlgorithms()
             obj.fft_prepare_swap_indices(2**i)
             self.assertEqual(obj.fft_swap_indices, expected)
+
+    def test_fft_prepare_lengths_list_32(self):
+        expected = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768,
+                    65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216,
+                    33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648,
+                    4294967296]
+        obj = fast_fourier_transform.MathAlgorithms()
+        limit = 32
+        for i in range(1, limit + 1):
+            obj.fft_prepare_lengths_list(2**i)
+            self.assertEqual(obj.fft_lengths, expected[:i])
