ccd2088dc2748127a61d69f893684c0fdde81240
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-02-13 09:53:02 -0600

    fixed test file to use the new imports

--- cheatsheetstuff/src/MathAlgos/math_algorithms_tests.py
+++ cheatsheetstuff/src/MathAlgos/math_algorithms_tests.py
@@ -30,21 +30,18 @@ class TestMathMethods(unittest.TestCase):
         test_limit = 2000
         obj = prime_tests.MathAlgorithms()
         test_obj = prime_tests.MathAlgorithms()
-        obj.sieve_of_eratosthenes(limit)
-        test_obj.sieve_of_eratosthenes(test_limit)
-        test_obj.primes_set = set(test_obj.primes_list)
-        primes_generated_by_sieve = set(obj.primes_list)
+        obj.fill_primes_list_and_set(limit)
+        test_obj.fill_primes_list_and_set(test_limit)
         for number in range(limit):
             if test_obj.is_prime_optimized(number):
-                self.assertTrue(number in primes_generated_by_sieve)
+                self.assertTrue(number in obj.primes_set)
             else:
-                self.assertTrue(number not in primes_generated_by_sieve)
+                self.assertTrue(number not in obj.primes_set)
 
     def test_is_prime_trivial_up_to_1m(self):
         limit = 1000000
         obj = prime_tests.MathAlgorithms()
-        obj.sieve_of_eratosthenes(limit)
-        obj.primes_set = set(obj.primes_list)
+        obj.fill_primes_list_and_set(limit)
         for number in range(limit):
             self.assertEqual(obj.is_prime_optimized(number), obj.is_prime_trivial(number))
 
@@ -52,17 +49,16 @@ class TestMathMethods(unittest.TestCase):
         test_limit = 1000000
         limit = 1000000
         random_test_limit = 10
-        test_obj = prime_tests.MathAlgorithms()
-        test_obj.sieve_of_eratosthenes(test_limit)
-        test_obj.primes_set = set(test_obj.primes_list)
+        base_obj = prime_tests.MathAlgorithms()
+        base_obj.fill_primes_list_and_set(test_limit)
         obj = prime_tests.MathAlgorithms()
         obj.miller_rabin_primality_test_prep()
         for number in range(limit):
-            self.assertEqual(test_obj.is_prime_optimized(number),
+            self.assertEqual(base_obj.is_prime_optimized(number),
                              obj.miller_rabin_primality_test(number))
         for _ in range(random_test_limit):
             number = randint(2 ** 50, 2 ** 55)
-            self.assertEqual(test_obj.is_prime_trivial(number),
+            self.assertEqual(base_obj.is_prime_trivial(number),
                              obj.miller_rabin_primality_test(number), number)
 
     def test_sieve_of_eratosthenes_100(self):
