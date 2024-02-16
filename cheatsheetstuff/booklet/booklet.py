ffdbcf87fbe1a95315a4c065a4f29cf4728d73c8
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-02-16 14:31:25 -0600

    added in a citation for a function

--- cheatsheetstuff/booklet/booklet.py
+++ cheatsheetstuff/booklet/booklet.py
@@ -3409,6 +3409,8 @@ class book_v1_forgot_to_add(GraphAlgorithms):
             queue.append(v)
 
   def solve_tower_of_powers(self):
+    """Compressed solution for tower of powers 2 power harder from this link.
+    https://shorturl.at/qDFG9 russel dash"""
     from math import log10, log
     EPS = 1e-14
     LIM = 309
