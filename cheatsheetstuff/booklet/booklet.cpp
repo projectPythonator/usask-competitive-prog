49d53271670d365e9a3d22738e421f3808b46478
projectPythonator <200~danjkstraProtonAlt1@protonmail.com>
2024-09-01 12:05:28 -0600

    fixed prime varient sieves for tyypes comments and logic

--- cheatsheetstuff/booklet/booklet.cpp
+++ cheatsheetstuff/booklet/booklet.cpp
@@ -289,96 +289,96 @@ public:
 		}
 	}
 
-
-	void sieveOfMinPrimes(int limit) {  
-		/*Stores the min or max prime divisor for each number up to n.
-
-			Complexity: Time: O(max(n lnln(sqrt(n)), n)), Space : post call O(n)
-		*/
+	// Stores the min or max prime divisor for each number up to n.
+	//
+	//	Complexity: Time: O(max(n lnln(sqrt(n)), n)), Space : post call O(n)
+	void sieveOfMinPrimes(int32 limit) {
 		minPrimes.assign(limit + 1, 0);
 		iota(minPrimes.begin(), minPrimes.end(), 0);
-		for (int two = 2; two <= limit; two += 2)
+		for (int32 two = 2; two <= limit; two += 2)
 			minPrimes[two] = 2;
-		for (int prime = 3; prime * prime <= limit; prime += 2)
-			if (minPrimes[prime] == prime)  // we found a prime
-				for (int multiple = prime * prime; multiple <= limit; multiple += (2 * prime))
-					if (minPrimes[multiple] == multiple) // min not set yet
+		for (int32 prime = 3; prime * prime <= limit; prime += 2)
+			if (minPrimes[prime] == prime)  // improvised prime bool check
+				for (int32 multiple = prime * prime; multiple <= limit; multiple += (2 * prime))
+					if (minPrimes[multiple] == multiple) // min not set this was than not checking
 						minPrimes[multiple] = prime;
 	}
-	void sieveOfEratosthenesVariants(int n_inclusive) {
-		/*Seven variants of prime sieve listed above.
 
-			Complexity :
-				function 1 : Time : O(n log(n)), Space : O(n)
-				function 2 : Time : O(n lnln(n)), Space : O(n)
-				function 3 : Time : O(n lnln(n) log(n)), Space : O(n)
-				function 4 : Time : O(n lnln(n) log(n)), Space : O(n)
-			*/
-		euler_phi_plus_sum_and_number_of_diff_prime_factors(n_inclusive);
-		num_and_sum_of_divisors(n_inclusive);
-		num_and_sum_of_prime_factors(n_inclusive);
+	// Seven variants of prime sieve listed above.
+	//
+	//	Complexity :
+	//	function 1 : Time : O(n log(n)), Space : O(n)
+	//	function 2 : Time : O(n lnln(n)), Space : O(n)
+	//	function 3 : Time : O(n lnln(n) log(n)), Space : O(n)
+	//	function 4 : Time : O(n lnln(n) log(n)), Space : O(n)
+	void sieveOfEratosthenesVariants(int32 limit) {
+		euler_phi_plus_sum_and_number_of_diff_prime_factors(limit);
+		num_and_sum_of_divisors(limit);
+		num_and_sum_of_prime_factors(limit);
 	}
 
-	void numAndSumOfDivisors(int limit) {
-		//  Does a basic sieve. Complexity function 1.
+	// Does a basic sieve. Complexity function 1.
+	void numAndSumOfDivisors(int32 limit) {
 		numDiv.assign(limit + 1, 1);
 		sumDiv.assign(limit + 1, 1ll);  // likely needs to be long long
-		for (int divisor = 2; divisor <= limit; divisor++)
-			for (int multiple = divisor; multiple <= limit; multiple += divisor) {
+		for (int32 divisor = 2; divisor <= limit; divisor++)
+			for (int32 multiple = divisor; multiple <= limit; multiple += divisor) {
 				numDiv[multiple]++;
 				sumDiv[multiple] += divisor;
 			}
 	}
 
-	void eulerPhiPlusSumAndNumOfDiffPrimeFactors(int limit) {
-		// This is basically same as sieve just using different ops. Complexity function 2.
+	// This is basically same as sieve just using different ops. Complexity function 2.
+	void eulerPhiPlusSumAndNumOfDiffPrimeFactors(int32 limit) {
 		numDiffPF.assign(limit + 1, 0);
 		sumDiffPF.assign(limit + 1, 0ll);	// likely needs to be long long
-		phi.assign(limit + 1, 0ll);			// likely needs to be long long
+		phi.assign(limit + 1, 0ll);				// likely needs to be long long
 		iota(phi.begin(), phi.end(), 0);
-		for (int prime = 2; prime <= limit; prime++)
-			if (numDiffPF[prime] == 0)
-				for (int multiple = prime; multiple <= limit; multiple += prime) {
+		for (int32 prime = 2; prime <= limit; prime++)
+			if (0 == numDiffPF[prime])
+				for (int32 multiple = prime; multiple <= limit; multiple += prime) {
 					numDiffPF[multiple]++;
 					sumDiffPF[multiple] += prime;
 					phi[multiple] = (phi[multiple] / prime) * (prime - 1);
 				}
 	}
 
-	void numAndSumOfPrimeFactors(int limit) {
-		// This uses similar idea to sieve but avoids divisions. Complexity function 3.
+	// This uses similar idea to sieve but avoids divisions. Complexity function 3.
+	void numAndSumOfPrimeFactors(int32 limit) {
 		numPF.assign(limit + 1, 0);
 		sumPF.assign(limit + 1, 0ll);	// likely needs to be long long
-		for (int prime = 2; prime <= limit; prime++)
-			if (numDiffPF[prime] == 0) {
-				int exponentLimit = 0;	// p^n | p^n <= limit < p^(n+1)
-				long long primePows[32];	// 32 limits us to 2^31 (2^0 == 1)
-				for (long long primeToN = 1; primeToN <= limit; primeToN *= prime)
+		for (int32 prime = 2; prime <= limit; prime++)
+			if (0 == numDiffPF[prime]) {
+				int32 exponentLimit = 0;	// p^n | p^n <= limit < p^(n+1)
+				int64 primePows[32];			// 32 limits us to 2^31 (2^0 == 1), increase if needed
+				for (int64 primeToN = 1; primeToN <= limit; primeToN *= prime)
 					primePows[exponentLimit++] = primeToN;
-				for (int exponent = 1; exponent < exponentLimit; exponent++) {
-					int primeToN = primePows[exponent];
-					for (int multiple = primeToN; multiple <= limit; multiple += primeToN) {
+				for (int32 exponent = 1; exponent < exponentLimit; exponent++) {
+					int32 primeToN = primePows[exponent];	// WARNING assumption that limit = int32
+					for (int32 multiple = primeToN; multiple <= limit; multiple += primeToN) {
 						numPF[multiple]++;
 						sumPF[multiple] += prime;
 			} } } // 3 closing brackets
 	}
 
-
-	void numAndSumOfDivisorsFaster(int limit) {
-		// This uses similar idea to sieve but avoids divisions. Complexity function 4
+	// This uses similar idea to sieve but avoids divisions. Complexity function 4
+	void numAndSumOfDivisorsFaster(int32 limit) {
 		// sumDiv.assign(limit + 1, 1ll);  // likely needs to be long long
 		numDiv.assign(limit + 1, 1);
 		curPow.assign(limit + 1, 1); // here a
-		for (int prime = 2; prime <= limit; prime++)
-			if (numDiv[prime] == 1) {
-				int exponentLimit = 0; long long primePows[32];
-				for (long long primeToExp = 1; primeToExp <= limit; primeToExp *= prime)
+		for (int32 prime = 2; prime <= limit; prime++)
+			if (1 == numDiv[prime]) {
+				int32 exponentLimit = 0;	// p^n | p^n <= limit < p^(n+1)
+				int64 primePows[32];			// 32 limits us to 2^31 (2^0 == 1), increase if needed
+				for (int64 primeToExp = 1ll; primeToExp <= limit; primeToExp *= prime)
 					primePows[exponentLimit++] = primeToExp;
-				// primePows[exponentLimit] = primePows[exponentLimit-1] * prime; / use if calculating sumDiv
-				for (int exponent = 1; exponent < exponentLimit; exponent++)
-					for (int mul = primePows[exponent], primeToN = primePows[exponent]; mul <= limit; mul += primeToN)
+				// primePows[exponentLimit] = primePows[exponentLimit-1]*prime; // used in sumDiv
+				for (int32 exponent = 1; exponent < exponentLimit; exponent++) {
+					int32 primeToN = primePows[exponent];	// WARNING assumption that limit = int32
+					for (int32 mul = primeToN; mul <= limit; mul += primeToN)
 						curPow[mul]++;
-				for (int multiple = prime; multiple <= limit; multiple += prime) {
+				}
+				for (int32 multiple = prime; multiple <= limit; multiple += prime) {
 					// sumDiv[multiple] *= ((primePows[curPow[multiple]] - 1) / (prime - 1));
 					numDiv[multiple] *= curPow[multiple];
 					curPow[multiple] = 1; // needs to happen regardless of version you are using
